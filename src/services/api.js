0c7645f5fb80a1f6bb47600f21ab9a5f714ae4bb
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-12-11 22:45:10 -0500

    chore: remove api-base-url console.log

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,6 @@ import axios from "axios";
 const API_VERSION = "v1";
 const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || "/api") + "/" + API_VERSION;
 
-console.log("API Base URL:", API_BASE_URL);
-
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
